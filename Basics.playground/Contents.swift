daedf56dbdc0e02b1bd33e36d9028e605bddc1e1
Reetika Mahajan <reetika.mahajan06@gmail.com>
2017-07-26 23:58:15 +0530

    Swift basics

---
+++ Basics.playground/Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+//Constant Declaration
+let name : String
+name = "Reetika"
+
+// Variable Declaration
+var age = 24
+var salary = 1_00_000
+
+
+// Keyword usage
+var printer = "Keyword"
+var `func` = "function"
+
+// Multi variable declaration in a asingle line
+var rollNo, address : String
+
+// Print
+print("age is \(age)","Name is \(name)", separator:". ", terminator: " EOF")
+
+//min and max Int
+Int.min
+Int.max
+
+//Numeral literals
+let decimal = 5
+let  binary = 0b101
+let octal = 0o5
+let hexa = 0x5
+
+// type alias
+typealias feet = Float
+
+// Tuples
+let person = (name : "Reetika", age : "28")
+person.0
+person.age
+
+var (sname, _) = ("Ram", "20")
+sname
+
+//Array
+let nameArray = ["Reetika", "28"]
+
+
+
